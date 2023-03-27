ba681cd900673592770bc8e0d2529bbad1ff277f
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-27 11:01:20 +0530

    variables in javascript

---
+++ variables.js
@@ -0,0 +1,36 @@
+//variable declarations in javascript
+/*
+rules for creating variable in javascript
+All JavaScript variables must be identified with unique names.
+
+These unique names are called identifiers.
+
+Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+The general rules for constructing names for variables (unique identifiers) are:
+
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we will not use it in this tutorial).
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+
+JavaScript identifiers are case-sensitive.
+*/
+//using nothing to create variable in javascript
+a=1
+b=2
+console.log(a)
+console.log(b)
+//using let to create variables
+let x=20
+let y=50
+console.log(x+y)
+//using const to crete variables
+const x1=20
+const y1=80
+console.log(x1+y1)
+//using var to create variables
+var x2=20
+var y2=80
+console.log(x2+y2)
