089c955fc6e22777a3ae965d919e45cdee644e12
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-27 13:26:57 +0530

    let keyword demonstrations

--- let.js
+++ let.js
@@ -16,6 +16,35 @@ console.log(x)
 var x1=30
 console.log(x1)
 // redeclare
-var x2=30
-console.log(x2)
+var x1=50
+console.log(x1)
+//block scope undertsnding
+{
+    let y=3
+}
+//y can not be used here
+//with var we can use it outside the block
+{
+    var y2=3
+}
+console.log(y2)
+//redeclaring with var imposes problems
+var z=10
+console.log(z)
+{
+    var z=20
+    console.log(z)
+}
+console.log("outside the block value changes")
+console.log(z)
+//redeclaring with let corrects this 
+let k=10
+console.log(k)
+{
+    let k=20
+    console.log("new value inside block")
+    console.log(k)
+}
+console.log("outside the block value does not changes")
+console.log(k)
 
