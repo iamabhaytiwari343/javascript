a80df341db3262f3873405f6b8fc59addffe923f
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-28 01:38:43 +0530

    basic demonstration of math object

---
+++ deg_to_radian.js
@@ -0,0 +1,13 @@
+//degree to radian
+function degToRad(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+  // radian to degree
+  function radToDeg(rad) {
+    return rad / (Math.PI / 180);
+  }
+
+  console.log(degToRad(165))
+  console.log(radToDeg(2))
+
+  
---
+++ recurr_alternate_random.js
@@ -0,0 +1,7 @@
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
+  
+  console.log(random(1, 10));
+  
