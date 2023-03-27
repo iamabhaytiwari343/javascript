53138c31f59ad9aaf07fed269a42dc1a2a33d1d9
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-27 11:11:09 +0530

    variable in js

--- variables.js
+++ variables.js
@@ -71,4 +71,8 @@ console.log(e)
 var carName1 = "Volvo";
 var carName1;
 //You cannot re-declare a variable declared with let or const.
+//JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names:
 
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 5;
