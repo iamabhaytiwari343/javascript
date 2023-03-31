5aabf9a414ad9896f8ca3d6853fe6370d3245575
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-31 21:29:41 +0530

    constructor in js

---
+++ constructor.js
@@ -0,0 +1,46 @@
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
+  myFather.nationality = "English";
+  myFather.name = function () {
+    return this.firstName + " " + this.lastName;
+  };
+  Person.nationality = "English";
+  function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+    this.nationality = "English";
+  }
+  function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+    this.name = function() {
+      return this.firstName + " " + this.lastName;
+    };
+  }
+  function Person(firstName, lastName, age, eyeColor) {
+    this.firstName = firstName; 
+    this.lastName = lastName;
+    this.age = age;
+    this.eyeColor = eyeColor;
+    this.changeName = function (name) {
+      this.lastName = name;
+    };
+  }
+//   new String()    // A new String object
+// new Number()    // A new Number object
+// new Boolean()   // A new Boolean object
+// new Object()    // A new Object object
+// new Array()     // A new Array object
+// new RegExp()    // A new RegExp object
+// new Function()  // A new Function object
+// new Date()      // A new Date object
