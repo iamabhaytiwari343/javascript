3b6ff9755ec34748c804f78faa1bce84bb9455d1
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-31 12:31:44 +0530

    string object in js

---
+++ string.js
@@ -0,0 +1,30 @@
+// Create a new string
+var myString = "hello world";
+
+// Get the length of the string
+var length = myString.length;
+
+// Convert the string to uppercase
+var uppercaseString = myString.toUpperCase();
+
+// Find the index of a substring
+var index = myString.indexOf("world");
+
+// Replace a substring with another string
+var newString = myString.replace("world", "everyone");
+
+var myString = "Hello, world!";
+var length = myString.length; // 13
+
+var uppercaseString = myString.toUpperCase(); // "HELLO, WORLD!"
+
+var lowercaseString = myString.toLowerCase(); // "hello, world!"
+
+var character = myString.charAt(1); // "e"
+
+var subset = myString.substring(1, 4); // "ell"
+
+var words = myString.split(" "); // ["Hello,", "world!"]
+
+var index = myString.indexOf("world"); // 7
+
