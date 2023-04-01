453f72b5af5f0531fc2c0a6b2932dd7fc3624a26
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-01 13:01:58 +0530

    type cating in js

--- typecating.js
+++ typecating.js
@@ -2,6 +2,16 @@
 let str = "123";
 let num = Number(str); // num is now a number with a value of 123
 //implicit type casting
-let num1 = 5;
-let str1 = "10";
+let num2 = 5;
+let str2 = "10";
 let result = num1 + str1; // result is now a string with a value of "510"
+
+//type conversion
+let str1 = "123";
+let num1 = Number(str);
+//coercion
+let num4 = 5;
+let str4 = "10";
+let result4 = num1 + str1; // result is now a string with a value of "510"
+
+
