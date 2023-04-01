5536447378e70e853634d595586718983c1cfc9d
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-01 12:57:41 +0530

    type casting

---
+++ typecating.js
@@ -0,0 +1,7 @@
+//explicit type casting in js
+let str = "123";
+let num = Number(str); // num is now a number with a value of 123
+//implicit type casting
+let num1 = 5;
+let str1 = "10";
+let result = num1 + str1; // result is now a string with a value of "510"
