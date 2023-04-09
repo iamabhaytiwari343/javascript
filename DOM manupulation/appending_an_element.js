399fea5339963e9282ea6a81c79f93f600c4ed94
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-09 22:54:03 +0530

    appending an element in html dom

--- DOM manupulation/appending_an_element.js
+++ DOM manupulation/appending_an_element.js
@@ -1,3 +1,9 @@
+// We have created the element and also has stored the reference of the element in a variable, but the created element is floating aimlessly because DOM does not know about the created element till now.
+// To make the created element a part of the document we have to explicitly attach it to the document using some methods.
+
+// append() - It appends the Node objects or DOMString object to the parent element.
+// appendChild() - It can append only Node objects to the parent element.
+
 function addChild() {
     const parent = document.getElementById("parent"); // selecting parent
     const child = document.createElement("p"); // creating child
@@ -7,3 +13,5 @@ function addChild() {
     parent.append(child);
     // you can also use appendChild()
 }
+// The reference of Node element which has to be appended is passed as an argument to the method. The method calling element is known as the parent while the element which is appended is called the child.
+
