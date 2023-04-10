8f10c1413f2e4fece4246e397498d9d78b057233
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-10 21:45:55 +0530

    removing attribute

---
+++ DOM manupulation/removing_attribute.js
@@ -0,0 +1,8 @@
+// You can also remove attributes from an HTML element using the removeAttribute() method in JavaScript.
+
+//To remove an attribute call the method on the element and pass the attribute name as an argument.
+
+var element = document.getElementById("element");
+
+// removing attribute
+element.removeAttribute("title");
