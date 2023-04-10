aa33b271914a0561a16fc2b233f30eed3727e747
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-10 21:35:30 +0530

    changing style of element

---
+++ DOM manupulation/changing_style_of_element.js
@@ -0,0 +1,17 @@
+//  You can also change the style of an element dynamically on an action. To change the style of an element you can use use the style property of the element.
+
+// With the style property, you can control any CSS property of the element like color, font, border, etc.
+
+// Syntax: element.style.property = value;
+
+// element - The element whose style is to be changed.
+// property - The property of the element whose value is to be changed.
+// value - The value of the property.
+// Here is an example that changes the color of the element:
+
+// selecting element
+const element = document.getElementById("element");
+
+// changing color of element
+element.style.color = "red";
+
