72a801058af61e421f545cb082726cf0faff21cc
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-10 21:38:26 +0530

    adding attribute

---
+++ DOM manupulation/adding_attribute_of_element.js
@@ -0,0 +1,14 @@
+// You can add an HTML attribute to an element using the setAttribute() method.
+// syntax: element.setAttribute(attribute, value);
+
+// element - The element whose attribute is to be added.
+// attribute - The attribute of the element.
+// value - The value of the attribute.
+// Here is an example that adds the attribute title to the element:
+
+// selecting element
+const element = document.getElementById("element");
+
+// adding attribute
+element.setAttribute("title", "This is a title");
+
