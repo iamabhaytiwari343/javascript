91e027a4453303a0de46cb33f40c7f4f28eddb20
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-04-12 22:55:44 +0530

    promise implementation

--- Promises/implementation.js
+++ Promises/implementation.js
@@ -14,3 +14,19 @@ function Promise(executor) {
   if (executor === noop) return;
   doResolve(executor, this);
 }
+
+/*
+Looks like a simple function with some properties initialized to 0 or null. Here are a few things to notice:
+this._state property can have three possible values as described above:
+
+0 - pending
+1 - fulfilled with _value
+2 - rejected with _value
+3 - adopted the state of another promise, _value
+
+Its value is0 (pending) when you create a new promise.
+Later doResolve(executor, this) is invoked with executor and promise object.
+Let’s move on to the definition of doResolve and see how it’s implemented.
+
+
+*/
